7219d6a99c5945d7c4e284810c67a133cb4f925a
Anuradhadesilva <anuradhashashimal2@gmail.com>
2024-01-18 10:08:23 +0530

    Create Login View

---
+++ ToDoList/Views/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  ToDoList
+//
+//  Created by Anuradha Desilva on 18/01/2024.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
