575d2f54a712cd75d66f16b2385c4d6f8dd92cad
Anuradhadesilva <anuradhashashimal2@gmail.com>
2024-01-24 11:37:00 +0530

    Add user profile

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -12,9 +12,43 @@ struct ProfileView: View {
     var body: some View {
         NavigationView{
             VStack{
-                
+                if let user = viewModel.user{
+                    Image(systemName: "person.circle")
+                         .resizable()
+                         .frame(width: 100,height: 100)
+                         .padding()
+                     VStack(alignment:.leading){
+                         HStack{
+                             Text("Name:")
+                                 .bold()
+                             Text(user.name)
+                         }
+                         HStack{
+                             Text("Email:")
+                                 .bold()
+                             Text(user.email)
+                         }
+                         HStack{
+                             Text("Member Since")
+                                 .bold()
+                             Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                         }
+                     }
+                     Button("Log Out"){
+                         viewModel.logOut()
+                     }
+                     .tint(.red)
+                     .padding()
+                     Spacer()
+                } else{
+                    Text("Loading profile......")
+                }
+               
             }
             .navigationTitle("Profile")
+        }
+        .onAppear{
+            viewModel.fetchUser()
         }
     }
 }
